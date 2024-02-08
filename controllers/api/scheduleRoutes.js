d00ad292ab74cf1b183dcfca54672e392a379b9c
Juan Acevedo <jmamartinez18@gmail.com>
2024-02-07 20:02:56 -0700

    added get to schedules

--- controllers/api/scheduleRoutes.js
+++ controllers/api/scheduleRoutes.js
@@ -53,3 +53,12 @@ router.delete('/:id', withAuth, async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/', withAuth, async(req, res) => {
+    try {
+        const eventData = await Schedules.findAll();
+        res.status(200).json(eventData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
