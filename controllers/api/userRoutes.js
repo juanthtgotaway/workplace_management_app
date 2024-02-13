1a4086053d7579683ea00ec03e7830be29ef1df3
MoonstruckOccultist <justgive1234@gmail.com>
2024-02-12 18:28:01 -0700

    fixed things added employee

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,58 +1,71 @@
 const router = require('express').Router();
-const bcrypt = require('bcrypt');
-const { User, DepEmployees } = require('../../models');
+const { User, Departments, DepEmployees } = require('../../models');
 
-// Route to render the login page
-router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-        // If the user is already logged in, redirect to the profile page
-        res.redirect('/profile');
-    } else {
-        res.render('login');
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll({
+            include: [{model: Departments, through: DepEmployees, as: 'users_department'}]
+        });
+        res.status(201).json(userData);
+    } catch (err) {
+        res.status(400).json(err);
     }
 });
 
-// Route to handle user login
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
 router.post('/login', async (req, res) => {
     try {
-        // Find the user by username
         const userData = await User.findOne({ where: { username: req.body.username } });
 
-        if (!userData || !bcrypt.compareSync(req.body.password, userData.password)) {
-            // If user not found or password is incorrect, render the login page with an error message
-            return res.status(400).render('login', { error: 'Incorrect username or password' });
+        if (!userData) {
+            res.status(400).json({ message: '❌ Incorrect email or password, please try again ❌' });
+            return;
         }
 
-        // Set session variables
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
+        const validPassword = await userData.checkPassword(req.body.password);
 
-        // Redirect to the profile page
-        res.redirect('/profile');
+        if (!validPassword) {
+            res.status(400).json({ message: '❌ Incorrect email or password, please try again ❌' });
+            return;
+        }
+
+        const isManagerC = userData.is_manager;
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            req.session.is_manager = isManagerC;
+
+            res.json({ user: userData, message: 'You are now logged in!' });
+        });
     } catch (err) {
-        // Render error page with an error message
-        res.status(500).render('error', { error: 'Internal Server Error' });
+        res.status(400).json(err);
     }
 });
 
-
-// Route to handle user logout
 router.post('/logout', (req, res) => {
-    // Destroy the session
-    req.session.destroy(() => {
-        // Redirect to the homepage after logout
-        res.redirect('/');
-    });
-});
-
-// Route to render the profile page
-router.get('/profile', (req, res) => {
     if (req.session.logged_in) {
-        // If the user is logged in, render the profile page
-        res.render('profile');
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
     } else {
-        // If the user is not logged in, redirect to the login page
-        res.redirect('/login');
+        res.status(404).end();
     }
 });
 
